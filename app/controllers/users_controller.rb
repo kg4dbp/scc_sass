7535fbbedd4a41bc0e27117ec6634d45aef6b4b2
Robert Crowder <kg4dbp@gmail.com>
2016-08-26 23:20:22 +0000

    added index action and added link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
+    
+    def index
+        
+    end
+    
     def show
         @user = User.find(params[:id])
         
     end
 end
+
+
